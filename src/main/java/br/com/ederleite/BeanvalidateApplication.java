bd51565c9475ff754be47fc83e0c6e09bbfb7605
Eder Leite <eder.leite@synchro.com.br>
2016-11-14 17:15:37 -0200

    first commit

---
+++ src/main/java/br/com/ederleite/BeanvalidateApplication.java
@@ -0,0 +1,12 @@
+package br.com.ederleite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanvalidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanvalidateApplication.class, args);
+	}
+}
